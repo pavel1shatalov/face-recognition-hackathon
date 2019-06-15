f2bc3e15c872a57d884d7b013733994bdbb80ff3
steklyanov <steklyanov@gmail.com>
2019-06-15 20:17:30 +0300

    ap

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response, request
+from flask import Flask, render_template, Response, request, jsonify
 # from camera import VideoCamera
 import time
 from flask_cors import CORS
@@ -22,13 +22,28 @@ def add_blog_ajax():
         write_photo(name, image)
         return "Done"
 
+@app.route('/admin', methods=['POST', 'GET'])
+def get_users_data():
+    if request.method == 'GET':
+        data = [
+            {
+                'name': 'Igor',
+                'available': '1',
+            },
+            {
+                'name': 'Sveta',
+                'available': '0',
+            }
+        ]
+        print(jsonify(data))
+        return (jsonify(data))
+
 def gen(camera):
     while True:
         time.sleep(0.5)
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
 
 @app.route('/video_feed')
 def video_feed():
