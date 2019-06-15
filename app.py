871c5f359227fe5ffe3db7597b726f12ea77bea2
steklyanov <steklyanov@gmail.com>
2019-06-15 12:00:26 +0300

    final mvp

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask, render_template, Response
+from camera import VideoCamera
 
-# configuration
-DEBUG = True
-
-# instantiate the app
 app = Flask(__name__)
-app.config.from_object(__name__)
 
-# enable CORS
-CORS(app)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# sanity check route
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-    return jsonify('pong!')
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
