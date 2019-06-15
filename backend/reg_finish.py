600de665c5c6a9c4ac684d8876e4366211c66b0f
0xd34b33f <0xd34b33f@users.noreply.github.com>
2019-06-15 21:39:03 +0300

    registration module added

--- backend/reg_finish.py
+++ backend/reg_finish.py
@@ -1,14 +1,18 @@
 import face_recognition_knn as fkn
 import cv2
 import face_recognition
+import pickle
 
 
 def finish_registraion():
+    knn_clf = fkn.train('./train_dir', model_save_path='faces_prediction_model.pckl', verbose=True)
     video_capture = cv2.VideoCapture(0)
-    fkn.train('./train_dir', model_save_path='faces_prediction_model.pckl', verbose=True)
     while True:
         ret, frame = video_capture.read()
-        rgb_frame = frame[:, :, ::-1]
-        face_locations = face_recognition.face_locations(rgb_frame)
-        face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
-        print(fkn.predict(None, face_locations, face_recognition, model_path='faces_prediction_model.pckl'))
+        cv2.imwrite('res.png', frame)
+        res = fkn.predict('res.png', knn_clf=knn_clf)
+        for name, (top, right, bottom, left) in res:
+            print("- Found {} at ({}, {})".format(name, left, top))
+
+
+finish_registraion()
